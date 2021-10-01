904ab3f2ac72b22969cef0a0b62596a4d18d515e
Biggarl6454 <90929826+Biggarl6454@users.noreply.github.com>
2021-09-30 23:20:45 -0400

    Add files via upload

---
+++ P2LAB1_Divide_BiggarLogan.py
@@ -0,0 +1,7 @@
+caffeine_mg = float(input())
+after6 = caffeine_mg / 2
+after12 = caffeine_mg / 4
+after24 = caffeine_mg / 16
+print('After 6 hours:', '%0.2f' % after6, 'mg')
+print('After 12 hours:', '%0.2f' % after12, 'mg')
+print('After 24 hours:', '%0.2f' % after24, 'mg')
